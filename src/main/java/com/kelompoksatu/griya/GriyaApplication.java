b6a3e3ba13dfa2bfa5aba78a2489cabe2c1c6aa4
Aditya Rizky Ramadhan <adityarizky1020@gmail.com>
2025-10-06 19:55:32 +0700

    init: add dependencies for project

---
+++ src/main/java/com/kelompoksatu/griya/GriyaApplication.java
@@ -0,0 +1,13 @@
+package com.kelompoksatu.griya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GriyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GriyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelompoksatu/griya/GriyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelompoksatu.griya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GriyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
