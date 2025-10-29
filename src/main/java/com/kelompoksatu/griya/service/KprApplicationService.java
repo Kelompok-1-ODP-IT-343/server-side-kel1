68ec5eb23f5075e09c006ae621d641233c3d3439
Aditya Rizky Ramadhan <adityarizky1020@gmail.com>
2025-10-29 19:22:14 +0700

    fix(KprApplicationService): add transactional annotation to createFirstApprovalWorkflow
    refactor(AssignWorkflowResponse): remove id field and use AllArgsConstructor

--- src/main/java/com/kelompoksatu/griya/service/KprApplicationService.java
+++ src/main/java/com/kelompoksatu/griya/service/KprApplicationService.java
@@ -748,6 +748,7 @@ public class KprApplicationService {
   }
 
   /** Create first approval workflow */
+  @Transactional
   private void createFirstApprovalWorkflow(Integer applicationId, Integer approvalStaffId) {
     ApprovalWorkflow workflow =
         ApprovalWorkflow.builder()
