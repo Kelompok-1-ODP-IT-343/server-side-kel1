2292f43d46c3f0fb788c980cc0a0e8f2e77e369b
Apriyandro Kusumo <apriyandro.trianditokusumo@mii.co.id>
2025-11-08 15:47:35 +0700

    update feature SystemNotification Entity spotless apply

--- src/main/java/com/kelompoksatu/griya/entity/NotificationChannel.java
+++ src/main/java/com/kelompoksatu/griya/entity/NotificationChannel.java
@@ -1,8 +1,8 @@
 package com.kelompoksatu.griya.entity;
 
 public enum NotificationChannel {
-    EMAIL,
-    SMS,
-    PUSH,
-    IN_APP
+  EMAIL,
+  SMS,
+  PUSH,
+  IN_APP
 }
--- src/main/java/com/kelompoksatu/griya/repository/SystemNotificationRepository.java
+++ src/main/java/com/kelompoksatu/griya/repository/SystemNotificationRepository.java
@@ -1,17 +1,16 @@
 package com.kelompoksatu.griya.repository;
 
 import com.kelompoksatu.griya.entity.SystemNotification;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface SystemNotificationRepository extends JpaRepository<SystemNotification, Long> {
 
-    // Mencari notifikasi berdasarkan user_id, diurutkan dari yang terbaru
-    List<SystemNotification> findByUserIdOrderByCreatedAtDesc(Integer userId);
+  // Mencari notifikasi berdasarkan user_id, diurutkan dari yang terbaru
+  List<SystemNotification> findByUserIdOrderByCreatedAtDesc(Integer userId);
 
-    // Menghitung notifikasi yang belum dibaca oleh user
-    long countByUserIdAndReadAtIsNull(Integer userId);
+  // Menghitung notifikasi yang belum dibaca oleh user
+  long countByUserIdAndReadAtIsNull(Integer userId);
 }
